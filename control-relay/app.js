2de0933c47125a427dc2eac95a41494d1ffe6cef
Dan Coupar <dan.coupar@gmail.com>
2023-03-28 21:13:58 +0100

    Add logging code on error

--- control-relay/app.js
+++ control-relay/app.js
@@ -37,6 +37,9 @@ function publish(command) {
 }
 
 function accept(req, res) {
+  req.on('error', function(err) {
+    console.log('error: ' + err);
+  }
   let urlParsed = url.parse(req.url, true);
 
   // Listen for subscribers
