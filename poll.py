c8f6e1a518ee58059c77d9bab036d0c35c6c6485
Dan Coupar <dan.coupar@gmail.com>
2023-10-12 23:09:49 +0100

    Rework error handling. Change log location.

--- poll.py
+++ poll.py
@@ -1,42 +1,51 @@
 #!/usr/bin/python
+import sys
+import os
 import logging
 import requests
-import os
 import time
 
 logging.basicConfig(
     level=logging.INFO,
     datefmt='%Y-%m-%d %H:%M:%S',
     format='%(asctime)-15s - [%(levelname)s] %(module)s: %(message)s',
-    handlers=[logging.FileHandler("blinds.log"), logging.StreamHandler()])
+    handlers=[logging.FileHandler("/usr/src/blinds/blinds.log"), logging.StreamHandler()])
 
-url = os.environ.get('CONTROL_RELAY_URI') + '/subscribe'
+try:
+    url = os.environ['CONTROL_RELAY_URI'] + '/subscribe'
+except:
+    logging.critical('unable to read environmental variable CONTROL_RELAY_URI')
+    sys.exit(1)
+
 err_count = 0
 bad_response_count = 0
 
 def poll():
     global err_count
     global bad_response_count
-    while (bad_response_count < 10):
-        logging.info('polling for command')
-        response = requests.get(url = url)
-        if (response.status_code == 200):
-            if (response.text == 'up' or response.text == 'down' or response.text == 'stop'):
-                logging.info('received command ' + response.text.upper())
-                os.system('python /usr/src/blinds/blinds.py ' + response.text)
-                bad_response_count = 0
-        else:
-            logging.error('received bad response: ' + str(response.status_code))
-            bad_response_count += 1
-        err_count = 0
+    while (True):
+        try:
+            logging.info('polling for command')
+            response = requests.get(url = url)
+            if (response.status_code == 200):
+                if (response.text == 'up' or response.text == 'down' or response.text == 'stop'):
+                    logging.info('received command ' + response.text.upper())
+                    os.system('python /usr/src/blinds/blinds.py ' + response.text)
+                    bad_response_count = 0
+            else:
+                logging.error('received bad response: ' + str(response.status_code))
+                bad_response_count += 1
+                if (bad_response_count > 9):
+                    logging.critical('aborted after ' + str(bad_response_count) + ' bad responses')
+                    sys.exit(1)
+                time.sleep(1)
+            err_count = 0
+        except Exception as err:
+            logging.error(str(err))
+            err_count += 1
+            if (err_count > 9):
+                logging.critical('aborted after ' + str(err_count) + ' errors')
+                sys.exit(1)
+            time.sleep(1)
 
-try:
-    poll()
-    logging.critical('aborted after ' + str(bad_response_count) + ' bad responses')
-    
-except Exception as err:
-    logging.error(str(err))
-    err_count += 1
-    if (err_count < 10):
-        time.sleep(10)
-        poll()
+poll()
